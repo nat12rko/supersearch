26bf4711c4489912b50f45a6d1a13807619d6c3b
Joachim Andersson <joachim.andersson@resurs.se>
2015-03-10 09:48:12 +0100

    Fixed so it dosn't cache the templates

--- web/src/main/webapp/component/components.js
+++ web/src/main/webapp/component/components.js
@@ -1,29 +1,37 @@
 ko.components.register("product-row", {
-    viewModel: { require: 'component/js/product-row.js'},
-    template: {require: 'text!../../component/html/product-row.html'}
+    viewModel: { require: 'component/js/product-row.js?timestamp='+new Date().getTime()},
+    template: {require: 'text!../../component/html/product-row.html?timestamp='+new Date().getTime()},
+    synchronous: true
 });
 ko.components.register("product-row-multiupplys", {
-    viewModel: { require: 'component/js/product-row-multiupplys.js'},
-    template: {require: 'text!../../component/html/product-row-multiupplys.html'}
+    viewModel: { require: 'component/js/product-row-multiupplys.js?timestamp='+new Date().getTime()},
+    template: {require: 'text!../../component/html/product-row-multiupplys.html?timestamp='+new Date().getTime()},
+    synchronous: true
+
 });
 ko.components.register("product-row-fraud", {
-    viewModel: { require: 'component/js/product-row-fraud.js'},
-    template: {require: 'text!../../component/html/product-row-fraud.html'}
+    viewModel: { require: 'component/js/product-row-fraud.js?timestamp='+new Date().getTime()},
+    template: {require: 'text!../../component/html/product-row-fraud.html?timestamp='+new Date().getTime()} ,
+    synchronous: true
 });
 ko.components.register("product-row-limit", {
-    viewModel: { require: 'component/js/product-row-limit.js'},
-    template: {require: 'text!../../component/html/product-row-limit.html'}
+    viewModel: { require: 'component/js/product-row-limit.js?timestamp='+new Date().getTime()},
+    template: {require: 'text!../../component/html/product-row-limit.html?timestamp='+new Date().getTime()},
+    synchronous: true
 });
 ko.components.register("product-row-ecommerce", {
-    viewModel: { require: 'component/js/product-row-ecommerce.js'},
-    template: {require: 'text!../../component/html/product-row-ecommerce.html'}
+    viewModel: { require: 'component/js/product-row-ecommerce.js?timestamp='+new Date().getTime()},
+    template: {require: 'text!../../component/html/product-row-ecommerce.html?timestamp='+new Date().getTime()} ,
+    synchronous: true
 });
 ko.components.register("span-date", {
-    viewModel: { require: 'component/js/span-date.js'},
-    template: {require: 'text!../../component/html/span-date.html'}
+    viewModel: { require: 'component/js/span-date.js?timestamp='+new Date().getTime()},
+    template: {require: 'text!../../component/html/span-date.html?timestamp='+new Date().getTime()} ,
+    synchronous: true
 });
 ko.components.register("span-search", {
-    viewModel: { require: 'component/js/span-search.js'},
-    template: {require: 'text!../../component/html/span-search.html'}
+    viewModel: { require: 'component/js/span-search.js?timestamp='+new Date().getTime()},
+    template: {require: 'text!../../component/html/span-search.html?timestamp='+new Date().getTime()} ,
+    synchronous: true
 });
 
