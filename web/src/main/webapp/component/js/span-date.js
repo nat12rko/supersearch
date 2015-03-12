1b806620d8aad2ab43bd2530e17cacc93880f4b9
Daniel Karlberg <daniel.karlberg@resurs.se>
2015-03-12 16:58:04 +0100

    Fancy timestamp in search view.

--- web/src/main/webapp/component/js/span-date.js
+++ web/src/main/webapp/component/js/span-date.js
@@ -8,9 +8,27 @@ define(['knockout'], function (ko) {
             } else if (data.mode === "date") {
                 self.date = new Date(self.raw).customFormat('#YYYY#-#MM#-#DD#');
             } else if (data.mode === "time") {
-                self.date = new Date(self.raw).customFormat('#hh#:#mm#:#ss#');
+                self.date = new Date(self.raw).customFormat('#hhh#:#mm#:#ss#');
+            } else if (data.mode === "fancy"){
+
+                var today = new Date();
+                var yesterday = new Date();
+                yesterday.setDate(today.getDate()-1);
+
+                var todayForCompare = today.customFormat('#YYYY#-#MM#-#DD#');
+                var yesterdayForCompare = yesterday.customFormat('#YYYY#-#MM#-#DD#');
+
+                var source = new Date(self.raw).customFormat('#YYYY#-#MM#-#DD#');
+
+                if (todayForCompare == source) {
+                    self.date = 'Today @' + new Date(self.raw).customFormat(' #hhh#:#mm#:#ss#');
+                } else if (yesterdayForCompare == source) {
+                    self.date = 'Yesterday @' + new Date(self.raw).customFormat(' #hhh#:#mm#:#ss#');
+                } else {
+                    self.date = new Date(self.raw).customFormat('#YYYY#-#MM#-#DD# @ #hhh#:#mm#:#ss#');
+                }
             } else {
-                self.date = new Date(self.raw).customFormat('#YYYY#-#MM#-#DD# #hh#:#mm#:#ss#');
+                self.date = new Date(self.raw).customFormat('#YYYY#-#MM#-#DD# #hhh#:#mm#:#ss#');
             }
         }
     }
