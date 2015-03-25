4b6440676b605e52b282a90b6f422a585a52f9ad
Daniel Karlberg <daniel.karlberg@resurs.se>
2015-03-25 12:39:19 +0100

    * #46982 Predefined queryes  - Added predefined querys, enebled bu alt-q  - Change font size in result list.  - Other UI improvments

---
+++ web/src/main/webapp/js/application/predefined_querys.js
@@ -0,0 +1,37 @@
+
+function searchFrozenPayments() {
+
+    searchViewModel.clearFilters();
+    searchViewModel.clearTimeSpan();
+    searchViewModel.addFilterNoSearch("annulled", "F");
+    searchViewModel.addFilterNoSearch("payment.lifePhase", "BOOKED_FROZEN");
+    searchViewModel.search();
+}
+
+function searchDebitedToday() {
+
+    var today = new Date();
+    var todayString = today.customFormat('#YYYY#-#MM#-#DD#');
+
+    searchViewModel.clearFilters();
+    searchViewModel.addFilterNoSearch("debited", "true");
+    searchViewModel.addFilterNoSearch("annulled", "false");
+    searchViewModel.setTimeSpan(todayString, todayString);
+
+    searchViewModel.search();
+}
+
+function searchDebitedYesterday() {
+    var today = new Date();
+    var yesterday = new Date();
+    yesterday.setDate(today.getDate()-1);
+
+    var yesterdayString = yesterday.customFormat('#YYYY#-#MM#-#DD#');
+
+    searchViewModel.clearFilters();
+    searchViewModel.addFilterNoSearch("debited", "true");
+    searchViewModel.addFilterNoSearch("annulled", "false");
+    searchViewModel.setTimeSpan(yesterdayString, yesterdayString);
+
+    searchViewModel.search();
+}
