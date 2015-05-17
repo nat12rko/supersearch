c743a8dc5b6d3396a90b98483dd6aa639a0d472f
Joachim Andersson <joachim.andersson@resurs.se>
2015-05-17 13:26:31 +0200

    Docker Startupscript

--- docker/startup/startup.sh
+++ docker/startup/startup.sh
@@ -1,6 +1,8 @@
-sudo docker stop ${branchname}.${docker.servicename}.web
-sudo docker stop ${branchname}.${docker.servicename}.rest
-sudo docker rm ${branchname}.${docker.servicename}.web
-sudo docker rm ${branchname}.${docker.servicename}.rest
-sudo docker run -d --name=${branchname}.${docker.servicename}.web ${branchname}.${docker.servicename}.web
-sudo docker run -d --name=${branchname}.${docker.servicename}.rest ${branchname}.${docker.servicename}.rest
+#!/bin/bash
+
+echo | ssh supervisor@${DOCKER_HOST} sudo docker stop ${branchname}.${docker.servicename}.web
+echo | ssh supervisor@${DOCKER_HOST} sudo docker stop ${branchname}.${docker.servicename}.rest
+echo | ssh supervisor@${DOCKER_HOST} sudo docker rm ${branchname}.${docker.servicename}.web
+echo | ssh supervisor@${DOCKER_HOST} sudo docker rm ${branchname}.${docker.servicename}.rest
+echo | ssh supervisor@${DOCKER_HOST} sudo docker run -d --name=${branchname}.${docker.servicename}.web ${branchname}.${docker.servicename}.web
+echo | ssh supervisor@${DOCKER_HOST} sudo docker run -d --name=${branchname}.${docker.servicename}.rest ${branchname}.${docker.servicename}.rest
