d81bcd1e63fa9ff3d77f30fae62d994e836bd203
Joachim Andersson <joachim.andersson@resurs.se>
2015-07-08 20:15:18 +0200

    Updated for new Docker Plugin

--- web/src/main/webapp/js/application/views.js
+++ web/src/main/webapp/js/application/views.js
@@ -1,7 +1,7 @@
 /**var baseUrl = "http://localhost:8080/rest/";**/
 /**var baseUrl = "http://supersearch.pte.loc/rest/";**/
 
-var baseUrl = "http://rest.${docker.servicename}.${branchname}.${docker.hostname}/rest/"
+var baseUrl = "${image.supersearch-rest.url}"
 
 function SearchViewWidgetsModel() {
     var self = this;
