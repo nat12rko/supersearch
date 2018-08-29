8e56e8f02e1ea009e5059ba7e58425c53729cf7e
Robin Kollenman <Robin.Kollenman@resurs.se>
2018-08-29 12:57:00 +0200

    init

--- web/src/main/webapp/component/js/paymentandorapplicationupdate-info.js
+++ web/src/main/webapp/component/js/paymentandorapplicationupdate-info.js
@@ -7,12 +7,16 @@ define([], function() {
         self.paymentupdate = ko.observable();
         self.created=ko.observable();
         self.modified=ko.observable();
-        self.authorizedPaymentdiffs= ko.observable();
+        self.authorizedPaymentdiffs= ko.observableArray();
+        self.authorized=ko.observable();
         //self.description= ko.observable();
         var row;
+        var date;
         var cell1;
         var cell2;
+        var cell3;
         var table = document.getElementById("paytab");
+        var k;
 
 
         self.id.subscribe(function(newValue) {              //iD i back-end
@@ -22,18 +26,15 @@ define([], function() {
             var url = baseUrl+"paymentandorapplicationupdate/"+newValue;
             ajax(url, 'GET').done(function (data) {
                 for (var i = 0; i < data.length; i++) {
-                    var k=i;
-                    var j=0;
-                   // self.authorizedPaymentdiffs(data[i].object.payment.authorizedPaymentDiffs)
+                  k=i;
 
 
 
-                        self.paymentupdate(data[i].object)
+                        self.paymentupdate(data[i].object);
+                    date = new Date(self.paymentupdate.timestamp);
 
-                        var date= new Date(data[i].object.timestamp)
 
-
-                    if(date.getMinutes().toString().length<2 || date.getHours().toString().length<2) {
+                    if (date.getMinutes().toString().length < 2 || date.getHours().toString().length < 2) {
 
                         if (date.getHours().toString().length < 2) {
 
@@ -57,13 +58,13 @@ define([], function() {
                     }
 
 
-                    else{
+                    else {
 
                         self.created(date.toDateString() + " " + date.getHours() + ":" + date.getMinutes())
                     }
 
-                    date=new Date(data[i].object.modified)
-                    if(date.getMinutes().toString().length<2 || date.getHours().toString().length<2) {
+                    date = new Date(data[i].object.modified)
+                    if (date.getMinutes().toString().length < 2 || date.getHours().toString().length < 2) {
 
                         if (date.getHours().toString().length < 2) {
 
@@ -87,56 +88,95 @@ define([], function() {
                     }
 
 
-                    else{
+                    else {
 
                         self.modified(date.toDateString() + " " + date.getHours() + ":" + date.getMinutes())
                     }
 
 
 
-
-                    for (var i = 0; i < data[k].object.payment.authorizedPaymentDiffs.length; i++) {
-
-
-
-                         row= table.insertRow();
-                         cell1=row.insertCell();
-                         cell2=row.insertCell();
-                        cell1.innerHTML="Item Description";
-                        cell2.innerHTML=data[k].object.payment.authorizedPaymentDiffs[i].description;
-                         row= table.insertRow();
-                         cell1=row.insertCell();
-                         cell2=row.insertCell();
-                        cell1.innerHTML="ArticleNo";
-                        cell2.innerHTML=data[k].object.payment.authorizedPaymentDiffs[i].articleNo;
-                         row= table.insertRow();
-                         cell1=row.insertCell();
-                         cell2=row.insertCell();
-                         cell3=row.insertCell();
-                        cell1.innerHTML="Quantity";
-                        cell2.innerHTML=data[k].object.payment.authorizedPaymentDiffs[i].quantity.quantity;
-                        cell3.innerHTML= data[k].object.payment.authorizedPaymentDiffs[i].quantity.unit;
-                        row= table.insertRow();
-                         cell1=row.insertCell();
-                         cell2=row.insertCell();
-                        cell1.innerHTML="UnitAmountIncludingVat";
-                        cell2.innerHTML=data[k].object.payment.authorizedPaymentDiffs[i].unitAmountIncludingVat;
-                         row= table.insertRow();
-                         cell1=row.insertCell();
-                        cell2=row.insertCell();
-                        cell1.innerHTML="VatRate";
-                        cell2.innerHTML=data[k].object.payment.authorizedPaymentDiffs[i].vatRate;
-                         row= table.insertRow();
-                         cell1=row.insertCell();
-                         cell2=row.insertCell();
-                        cell1.innerHTML="TotalAmount";
-                        cell2.innerHTML=data[k].object.payment.authorizedPaymentDiffs[i].totalAmount;
-                        j=j+5;
+                    if(data[i].object.payment!=null && data[i].object.payment.authorizedPaymentDiffs!=null) {
+                        self.authorizedPaymentdiffs.removeAll();
+                        for (var i = 0; i < data[k].object.payment.authorizedPaymentDiffs.length; i++) {
+                            if (data[k].object.payment.authorizedPaymentDiffs[i] != null) {
+                                self.authorizedPaymentdiffs.push(data[k].object.payment.authorizedPaymentDiffs[i]);
 
 
+                            }
+                        }
 
                     }
+
+                    else{
+
+                        self.authorizedPaymentdiffs.removeAll();
+                    }
+
+
+
+
+/*
+
+if(data[k].object.payment!=null && data[k].object.payment.authorizedPaymentDiffs !=null) {
+                        if(table.rows.length>= 0) {
+                            for (var i = table.rows.length - 1; i > 0; i--) {
+                                table.deleteRow(i);
+                            }
+                        }
+    for (var i = 0; i < data[k].object.payment.authorizedPaymentDiffs.length; i++) {
+
+
+        row = table.insertRow();
+        cell1 = row.insertCell();
+        cell2 = row.insertCell();
+        cell1.innerHTML = "Item Description";
+        cell2.innerHTML = data[k].object.payment.authorizedPaymentDiffs[i].description;
+        row = table.insertRow();
+        cell1 = row.insertCell();
+        cell2 = row.insertCell();
+        cell1.innerHTML = "ArticleNo";
+        cell2.innerHTML = data[k].object.payment.authorizedPaymentDiffs[i].articleNo;
+        row = table.insertRow();
+        cell1 = row.insertCell();
+        cell2 = row.insertCell();
+        cell3 = row.insertCell();
+        cell1.innerHTML = "Quantity";
+        cell2.innerHTML = data[k].object.payment.authorizedPaymentDiffs[i].quantity.quantity;
+        cell3.innerHTML = data[k].object.payment.authorizedPaymentDiffs[i].quantity.unit;
+        row = table.insertRow();
+        cell1 = row.insertCell();
+        cell2 = row.insertCell();
+        cell1.innerHTML = "UnitAmountIncludingVat";
+        cell2.innerHTML = data[k].object.payment.authorizedPaymentDiffs[i].unitAmountIncludingVat;
+        row = table.insertRow();
+        cell1 = row.insertCell();
+        cell2 = row.insertCell();
+        cell1.innerHTML = "VatRate";
+        cell2.innerHTML = data[k].object.payment.authorizedPaymentDiffs[i].vatRate;
+        row = table.insertRow();
+        cell1 = row.insertCell();
+        cell2 = row.insertCell();
+        cell1.innerHTML = "TotalAmount";
+        cell2.innerHTML = data[k].object.payment.authorizedPaymentDiffs[i].totalAmount;
+
+
+
+    }
+}
+
+else{
+
+    if(table.rows.length>= 0) {
+        for (var i = table.rows.length - 1; i > 0; i--) {
+            table.deleteRow(i);
+        }
+    }
+}
+*/
+
                 }
+
+
             });
 
 
--- web/src/main/webapp/component/js/row-paymentandorapplicationupdate.js
+++ web/src/main/webapp/component/js/row-paymentandorapplicationupdate.js
@@ -2,7 +2,21 @@ define(['knockout'], function (ko) {
     function ProductRow(data) {
         var self = this;
         self.row = (data && data.row) || "none";
+        self.totalAmountifNull=  function() {
 
+            var totalamount = self.row.object.payment.authorization;
+
+
+            if (totalamount === null) {
+                return 0;
+
+            }
+
+            else{
+
+                return self.row.object.payment.authorization.amountWithVat
+            }
+        }
         self.show = function(){
             showpayupdate(self.row.object);
         }
